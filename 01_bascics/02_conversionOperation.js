619ab28790bfca9ec673076dc14ee4a4b351e877
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-07-24 10:51:08 +0000

    converting varibale from one form to anthore form

---
+++ 01_bascics/02_conversionOperation.js
@@ -0,0 +1,39 @@
+// ** Converting in number **
+
+/*let age =null;
+
+console.log(typeof(age));
+
+let ageInNum = Number(age)
+
+console.log(typeof ageInNum);  //op :- number
+console.log(ageInNum);  // Nan : age = 18ruk  */
+
+// While coverting below lieral into number getiing below output
+
+//age = "18ruk" Nan
+// age = null  op:- 0
+//age = undefined  op:- undefined
+//age = true op:- 1
+// age = false  op :- 0
+// age = "rupesh"   op :- Nan
+
+
+
+// ** CONVERTING IN BOOLEAN **
+
+// let userLogged = 1;  => true
+// let userLogged = "rupesh";  => true
+// let userLogged = ""; => false 
+/* let loggedIn = Boolean(userLogged)
+console.log(loggedIn); */
+
+
+
+//**  COVERNTING IN STRING **
+
+let num = 34;
+
+let stringNum = String(num);
+console.log(stringNum);
+console.log(typeof stringNum); // string
