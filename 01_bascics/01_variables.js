d8ee6a02ffe21d61380530ce61c5a7c38939a70f
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-07-24 07:53:06 +0000

    basics variables

---
+++ 01_bascics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 2239257;
+let accountEmail = "rupesh@gmail.com";
+var accountPass = "1234r";
+accountCity = "Nagpur";
+let accountState
+
+//accountId= 2239244  not allowed to redeclare in const keyword
+accountEmail = "ritesh@gmail"
+accountPass = "345r"
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPass);
+console.log(accountCity);
+console.table([accountEmail,accountPass,accountCity,accountState])
+
+/*
+ prfer not to usew var 
+ beacuswe of issue in block scope and functional scope
+*/
