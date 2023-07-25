105ee5bd982ec21cb9da50d2d024d6ddbf6eeeda
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-07-25 08:50:57 +0000

    number and math

---
+++ 01_bascics/03_nums_maths.js
@@ -0,0 +1,32 @@
+// let num =22;
+// console.log(num);
+
+// let score = new Number(400);
+// console.log(score);
+// console.log(score.toFixed(2));
+
+
+// let hundred =1000000000;
+// console.log(hundred.toLocaleString("en-IN"));
+
+
+// *************************Maths *******************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-400));
+// console.log(Math.round(4.5));
+
+console.log(Math.floor((Math.random()*10))+1);
+
+
+// let min =20;
+// let max=30;
+
+// console.log(Math.floor(Math.random()*(max- min +1))+ min);
+
+
+let a = 100;
+let b =200;
+
+console.log(Math.floor(Math.random()*(b-a + 1))+a)
