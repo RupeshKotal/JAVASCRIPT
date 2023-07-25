72cc3be7b69d923f9092a8c3dc7c6bc9c46d69dd
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-07-25 09:40:05 +0000

    dates and will explore later also

---
+++ 01_bascics/04_dates.js
@@ -0,0 +1,29 @@
+let date = new Date();
+// console.log(date.toString())
+
+// console.log(date.toISOString())
+
+// console.log(date.toDateString());
+
+// console.log(date.toTimeString());
+
+
+// console.log(date.toLocaleDateString());
+
+// console.log(date.toUTCString());
+
+// let createDate = new Date(2023,0,23)
+// console.log(createDate.toDateString());
+
+// let createDate = new Date(2023,0,23,5,10)
+// console.log(createDate.toLocaleString());
+
+// let createDate = new Date("01-14-2023")
+// console.log(createDate.toLocaleString());
+
+
+
+//======================================
+
+let newDate = new Date()
+console.log(newDate.getMonth());
