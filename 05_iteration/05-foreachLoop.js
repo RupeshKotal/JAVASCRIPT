2f7a541f43280d44f9d6fcb02277220caf659d0f
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-01 05:25:03 +0000

    for each loop

---
+++ 05_iteration/05-foreachLoop.js
@@ -0,0 +1,25 @@
+const coding =['javascript','java','ruby','python','Cpp']
+
+coding.forEach( function (item) {
+   // console.log(item);
+    
+} )
+
+coding.forEach( (val) => {
+   // console.log(val)
+} )
+
+
+// diffrent type
+
+function print(item){
+  // console.log(item)
+}
+coding.forEach(print)
+
+// example
+/*
+coding.forEach( (itms ,inde , arr) => {
+    console.log(itms, inde, arr)
+} )
+*/
