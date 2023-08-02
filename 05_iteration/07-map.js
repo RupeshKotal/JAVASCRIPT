ad23e86cdb91c9cf5c2e29343d09334066bb8b6d
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-02 05:15:51 +0000

    map function

--- 05_iteration/07-map.js
+++ 05_iteration/07-map.js
@@ -1,4 +1,18 @@
 const myNum = [1,2,3,4,5,6,7]
  const number = myNum.map( (num) => (num +10) )
 
- console.log(number)
+ //console.log(number)
+
+
+//  =================== Chaning ==============
+
+const newNums = [1,2,3,4,5,6,7,8,9,10]
+
+const chain = newNums
+              .filter( (num)=> num > 2 )
+              .map( (num) => num * 10 )
+              .filter((num) => num >= 50)
+              .map((num) => num + 1)
+console.log(chain)
+
+ 
